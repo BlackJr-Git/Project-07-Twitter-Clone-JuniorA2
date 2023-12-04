ba3452fbd524d9f20bd03a1977b2455fe38a7b51
BlackJr-Git <juniorassosa@gmail.com>
2023-12-04 15:36:45 +0100

    add : className props on tweet-action component and tweet-actions classes on css

--- src/components/tweet-content.jsx
+++ src/components/tweet-content.jsx
@@ -1,57 +1,59 @@
+import { TweetAction } from "../components";
 
-import {TweetAction } from "../components"
+function TweetContent({
+  texte,
+  imgsrc,
+  author,
+  userName,
+  time,
+  reply,
+  retweet,
+  react,
+}) {
+    return (
+      <div className="tweet-content">
+        <div className="tweet-body">
+          <div className="tweet-title">
+            <p className="tweet-title-author"> {author} </p>
+            <img src="src\images\Icons\Verified.png" alt="verfied icon" />
+            <p className="tweet-title-details">
+              {userName} . {time}
+            </p>
+          </div>
 
-function TweetContent({texte, imgsrc,author, userName, time , reply, retweet, react}) {
-            
-    if (imgsrc){
-        return (
-            
-            <div className="tweet-content">
-                <div className="tweet-body">
-                    
-                    <div className="tweet-title">
-                        <p className="tweet-title-author"> {author} </p>
-                        <img src="src\images\Icons\Verified.png" alt="verfied icon" />
-                        <p className="tweet-title-details">{userName} . {time}</p>
-                    </div>
+          <p className="tweet-text"> {texte} </p>
 
-                    <p className="tweet-text"> {texte} </p>
-                        
-                    <div className="tweet-image">
-                        <img src= {imgsrc} alt="" />
-                    </div>
-                </div>
-                
-                <div className="tweet-actions">
-                    <TweetAction imgsrc={"src\\images\\Icons\\Reply.png"} actionCount={reply} />
-                    <TweetAction imgsrc={"src\\images\\Icons\\Retweet.png"} actionCount={retweet} />
-                    <TweetAction imgsrc={"src\\images\\Icons\\React.png"} actionCount={react} />
-                    <TweetAction imgsrc={"src\\images\\Icons\\Share.png"} actionCount={null} />
-                </div>
-            </div>
-        )
-    } else {
-        return (
-            <div className="tweet-content">
-                <div className="tweet-body">
-                    
-                    <div className="tweet-title">
-                        <p className="tweet-title-author"> {author} </p>
-                        <img src="src\images\Icons\Verified.png" alt="verfied icon" />
-                        <p className="tweet-title-details">{userName} . {time}</p>
-                    </div>
+          {imgsrc &&  <div className="tweet-image">
+                                    <img src= {imgsrc} alt="" />
+                                </div> 
+                    }
+        </div>
 
-                    <p className="tweet-text"> {texte} </p>
-                </div>
-                
-                <div className="tweet-actions">
-                    <TweetAction imgsrc={"src\\images\\Icons\\Reply.png"} actionCount={reply} />
-                    <TweetAction imgsrc={"src\\images\\Icons\\Retweet.png"} actionCount={retweet} />
-                    <TweetAction imgsrc={"src\\images\\Icons\\React.png"} actionCount={react} />
-                    <TweetAction imgsrc={"src\\images\\Icons\\Share.png"} actionCount={null} />
-                </div>
-            </div>
-        ) }
-}  
+        <div className="tweet-actions">
+          <TweetAction
+            imgsrc={"src\\images\\Icons\\Reply.png"}
+            actionCount={reply}
+            className={'tweet-action tweet-reply'}
+          />
+          <TweetAction
+            imgsrc={"src\\images\\Icons\\Retweet.png"}
+            actionCount={retweet}
+            className={'tweet-action tweet-retweet'}
+          />
+          <TweetAction
+            imgsrc={"src\\images\\Icons\\React.png"}
+            actionCount={react}
+            className={'tweet-action tweet-react'}
+          />
+          <TweetAction
+            imgsrc={"src\\images\\Icons\\Share.png"}
+            actionCount={null}
+            className={"tweet-action"}
+          />
+        </div>
+      </div>
+    );
+  
+}
 
-export default TweetContent ;
+export default TweetContent;
--- src/components/tweets.jsx
+++ src/components/tweets.jsx
@@ -11,7 +11,7 @@ function Tweets() {
           tweetTime={"7m"}
           tweetAvatar={"src\\images\\tweet-profile-photo.png"}
           tweetText={'President Joe Biden touted a new agreement reached with the European Union to ease Trump-era tariffs on aluminum and steel as a "major breakthrough" that would serve to both strengthen the US steel industry and combat the global climate crisis.'}
-          reply = {57} retweet={144} react={184}
+          reply = {"57"} retweet={"144"} react={"184"}
         />
 
         <Tweet 
