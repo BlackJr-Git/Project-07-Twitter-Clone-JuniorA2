119ff14f843684b0259cf16e9de87f782126e042
BlackJr-Git <juniorassosa@gmail.com>
2023-12-06 17:03:32 +0100

    remove react-router-dom import

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { createBrowserRouter } from 'react-router-dom'
+// import { createBrowserRouter } from 'react-router-dom'
  
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
