374b42dd5d9132342bc544ef898ba717adf229f9
BlackJr-Git <juniorassosa@gmail.com>
2024-01-10 16:34:14 +0100

    feat : filter tweets data with username on profile page and set it to Tweets components

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -2,11 +2,15 @@ import { Header, ProfileHero, Tweets } from "../components";
 import { tweetData } from "../utils/tweet-data";
 
 function Profile() {
+
+    const user = '@twitter' ;
+    const tweetsOfUser = tweetData.filter(tweet => tweet.userName === user);
+
     return (
         <main className="timeline">
             <Header pageTitle={"Profile"} />
             <ProfileHero />
-            <Tweets tweetData={tweetData} />
+            <Tweets tweetData={tweetsOfUser} />
         </main>
     )
 }
