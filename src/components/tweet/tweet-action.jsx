0609fad89fe0c3ff66aa695d8dd4f363f4af2288
BlackJr-Git <juniorassosa@gmail.com>
2024-01-18 13:34:57 +0100

    style : like button on click

--- src/components/tweet/tweet-action.jsx
+++ src/components/tweet/tweet-action.jsx
@@ -1,10 +1,17 @@
 import SvgFile from "../../images/svg-file";
 
 function TweetAction({ actionCount, className, path, path2, handleClick, icone }) {
+  let actionIcon ;
+  if (icone) {
+    actionIcon = icone
+  } else {
+    actionIcon =<SvgFile path={path} path2={path2} /> 
+  }
+  
+  
   return (
     <div className={className} onClick={handleClick}>
-      {icone}
-      <SvgFile path={path} path2={path2} />
+      {actionIcon}
       <p>{actionCount}</p>
     </div>
   );
