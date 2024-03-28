bd79f63e39014a693621bdd165cf356b7a15a48d
Junior Asosa <juniorassosa@gmail.com>
2024-03-28 20:06:53 +0100

    fix : date formatter function when date is undifined

--- src/utils/date-formatter.js
+++ src/utils/date-formatter.js
@@ -1,37 +1,39 @@
 // let date = "Sat Mar 12 2024 21:15:41 GMT+0100 (West Africa Standard Time)"
 
 function dateFormatter(date) {
-  let dateInfo = date.split(" ");
-  let dateYears = +dateInfo[3];
-  let dateDays = dateInfo[2];
-  let dateMounth = dateInfo[1];
+  if (date) {
+    let dateInfo = date.split(" ");
+    let dateYears = +dateInfo[3];
+    let dateDays = dateInfo[2];
+    let dateMounth = dateInfo[1];
 
-  const totalTimeSincePosted = new Date(date);
-  const now = new Date();
-  let timeDifference = Math.abs(totalTimeSincePosted - now) / 1000 ; 
+    const totalTimeSincePosted = new Date(date);
+    const now = new Date();
+    let timeDifference = Math.abs(totalTimeSincePosted - now) / 1000;
 
-  const days = Math.floor(timeDifference / 86400);
-  timeDifference -= days * 86400;
+    const days = Math.floor(timeDifference / 86400);
+    timeDifference -= days * 86400;
 
-  const hours = Math.floor(timeDifference / 3600) % 24;
-  timeDifference -= hours * 3600;
+    const hours = Math.floor(timeDifference / 3600) % 24;
+    timeDifference -= hours * 3600;
 
-  const minutes = Math.floor(timeDifference / 60) % 60;
-  timeDifference -= minutes * 60;
+    const minutes = Math.floor(timeDifference / 60) % 60;
+    timeDifference -= minutes * 60;
 
-  let displayDate;
+    let displayDate;
 
-  if (now.getFullYear() - dateYears >= 1) {
-    displayDate = `${dateDays} ${dateMounth} ${dateYears}`;
-  } else if (days >= 1) {
-    displayDate = `${dateDays} ${dateMounth}`;
-  } else if (hours >= 1) {
-    displayDate = `${hours}h`;
-  } else {
-    displayDate = `${minutes}min`;
+    if (now.getFullYear() - dateYears >= 1) {
+      displayDate = `${dateDays} ${dateMounth} ${dateYears}`;
+    } else if (days >= 1) {
+      displayDate = `${dateDays} ${dateMounth}`;
+    } else if (hours >= 1) {
+      displayDate = `${hours}h`;
+    } else {
+      displayDate = `${minutes}min`;
+    }
+
+    return displayDate;
   }
-
-  return displayDate;
 }
 
 export default dateFormatter;
